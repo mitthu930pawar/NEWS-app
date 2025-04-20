f6f9f7209da3231334114efa4d62d899818fd892
mitalipawar <156992409+mitthu930pawar@users.noreply.github.com>
2025-04-20 12:05:09 +0530

    newsapp

---
+++ src/components/News.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Newsitem from './Newsitem';
+
+
+export class News extends Component {
+  render() {
+    return (
+      <>
+      <div>
+        < Newsitem/ >
+        < Newsitem/ >
+        < Newsitem/ >
+        < Newsitem/ >
+        < Newsitem/ >
+        < Newsitem/ >
+       </div>
+    
+       </>
+     
+    
+      
+    );
+  }
+}
+
+export default News;
